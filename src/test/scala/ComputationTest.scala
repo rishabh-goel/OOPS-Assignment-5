fdfc346e0b1ea77d44da1d2d8d120e9a57f74716
rishabhgoel <goel.rishabh12@gmaill.com>
2022-04-24 21:07:44 -0500

    Added testcases for PartialResult and Monadic function

--- src/test/scala/ComputationTest.scala
+++ src/test/scala/ComputationTest.scala
@@ -9,64 +9,157 @@ import scala.collection.mutable.{ListBuffer, Map, Set}
 class ComputationTest extends AnyFlatSpec with Matchers{
 
   // Test 1
-  it should "check the working of Else condition in If" in {
-    Assign("E", Value(Set(1,5,6))).eval()
-    If(Check(Variable("E"), Value(10)),
-      Then(Assign("E", Value(Set(1))), Assign("E", Value(Set(10))), Assign("E", Value(Set(100)))),
-      Else(Assign("E", Value(Set(1,5,6,10))), Assign("E", Value(Set(1,5,6,1000))))
-    ).eval()
-    assert(Variable("E").eval() === mutable.HashSet(1, 5, 6, 1000))
+  it should "check for partial and complete evaluation" in {
+    assert(Variable("x").eval() === Variable("x"))
+
+    Assign("x", Value(Set(1))).eval()
+    assert(Variable("x").eval() === mutable.HashSet(1))
+
+    Assign("x", Value(Set(1))).eval()
+    assert(Union(Variable("x"), Variable("Y")).eval() === Union(Value(mutable.HashSet(1)),Variable("Y")))
   }
 
   // Test 2
-  it should "check single level If condition failing with try catch" in {
+  it should "check monadic function for Union" in {
 
-    assert(Scope("myScope", Assign("E", Value(Set(1,2,3,4,5))),
-      ExceptionClassDef("ExceptionClass", Field("Reason")),
-      If(Check(Variable("E"), Value(6)), Then(Insert(Variable("E"), Value(6)), Insert(Variable("E"), Value(7))), Else(ThrowException(ExceptionClassDef("ExceptionClass"), "Check Failed"))),
-      Insert(Variable("E"), Value(8)),
-      Insert(Variable("E"), Value(9)),
-      CatchException(ExceptionClassDef("ExceptionClass"))
-    ).eval() === "Check Failed")
+    def func(input: SetExp) = {
+      input match {
+        case Value(x) => Value(x)
+        case Variable(x) => {
+          val output = Variable(x).eval()
+          val re = output match {
+            case p: PartialResult => Variable(x)
+            case a: Any => Value(a)
+          }
+          re
+        }
+
+        // Optimizations
+        // 1. Union(x, x) => Value(x)
+        // 2. Union(x, emptySet) => Value(x)
+        case Union(x, y) => {
+          if (x == y || y.eval() == mutable.HashSet())
+            x.eval()
+          else if (x.eval() == mutable.HashSet())
+            y.eval()
+          else
+            Union(x, y).eval()
+        }
+      }
+    }
+
+    Assign("x", Value(Set(1))).eval()
+    // Value of x is present
+    assert(MonadsOptimize(Union(Variable("x"), Variable("x"))).map(func) === mutable.HashSet(1))
+
+    // Value of a is not present
+    assert(MonadsOptimize(Union(Variable("a"), Variable("a"))).map(func) === Variable("a"))
   }
 
   // Test 3
-  it should "check single level If condition succeeding with try catch" in {
+  it should "check monadic function for Diff" in {
 
-    assert(Scope("myScope1", Assign("F", Value(Set(1,2,3,4,5))),
-      ExceptionClassDef("AnotherExceptionClass", Field("ErrorReason")),
-      If(Check(Variable("F"), Value(1)), Then(Insert(Variable("F"), Value(1)), Insert(Variable("F"), Value(7))), Else(ThrowException(ExceptionClassDef("AnotherExceptionClass"), "Check Failed"))),
-      Insert(Variable("F"), Value(8)),
-      Insert(Variable("F"), Value(9)),
-      CatchException(ExceptionClassDef("AnotherExceptionClass"))
-    ).eval() === mutable.HashSet(1, 2, 3, 4, 5, 7, 8, 9))
+    def func(input: SetExp) = {
+      input match {
+        case Value(x) => Value(x)
+        case Variable(x) => {
+          val output = Variable(x).eval()
+          val re = output match {
+            case p: PartialResult => Variable(x)
+            case a: Any => Value(a)
+          }
+          re
+        }
+
+        // Optimizations
+        // 1. Diff(x, x) => emptySet
+        // 2. Diff(x, emptySet) => Value(x)
+        case Diff(x, y) => {
+          if(x == y)
+            mutable.HashSet()
+          else if(x.eval() == mutable.HashSet())
+            y.eval()
+          else if(y.eval() == mutable.HashSet())
+            x.eval()
+          else
+            Diff(x, y).eval()
+        }
+      }
+    }
+
+    Assign("x", Value(Set(1,2,3,4))).eval()
+    // Value of x is present
+    assert(MonadsOptimize(Diff(Value(mutable.HashSet()), Variable("x"))).map(func) === mutable.HashSet(1, 2, 3, 4))
+
+    // Value of a is not present
+    assert(MonadsOptimize(Diff(Value(mutable.HashSet()), Variable("a"))).map(func) === Variable("a"))
   }
 
   // Test 4
-  it should "check for multi-level If condition" in {
-    Assign("E", Value(Set(1,5,6))).eval()
-    If(Check(Variable("E"), Value(1)),
-      Then(
-        If(Check(Variable("E"), Value(7)), Then(Assign("E", Value(Set(1,5,6,7)))), Else())
-      ),
-      Else(Assign("E", Value(Set(1,5,6,10))), Assign("E", Value(Set(1,5,6,1000))))
-    ).eval()
-    assert(Variable("E").eval() === mutable.HashSet(1, 5, 6))
+  it should "check monadic function for Intersect for complete evaluation" in {
+
+    def func(input: SetExp) = {
+      input match {
+        case Value(x) => Value(x)
+        case Variable(x) => {
+          val output = Variable(x).eval()
+          val re = output match {
+            case p: PartialResult => Variable(x)
+            case a: Any => Value(a)
+          }
+          re
+        }
+
+        // Optimizations
+        // 1. Intersect(x, x) => Value(x)
+        // 2. Intersect(x, emptySet) => emptySet
+        case Intersect(x, y) => {
+          if(x == y)
+            x.eval()
+          else if(x.eval() == mutable.HashSet() || y.eval() == mutable.HashSet())
+            mutable.HashSet()
+          else
+            Intersect(x, y).eval()
+        }
+      }
+    }
+
+    Assign("x", Value(Set(1))).eval()
+    // Value of x is present
+    assert(MonadsOptimize(Intersect(Variable("x"), Variable("x"))).map(func) === mutable.HashSet(1))
+    assert(MonadsOptimize(Intersect(Variable("x"), Value(mutable.HashSet()))).map(func) === mutable.HashSet())
   }
 
   // Test 5
-  it should "check if Scala try-catch replaced with Scaset try-catch" in {
-    assert(Interface("MyInterface",
-      Public(CreateMethod("m4", Params("a", "b"), Assign("c", Cross(Variable("a"), Variable("b"))), Variable("c")))
-    ).eval() === "Interface methods can't have body")
+  it should "check monadic function for Intersect for partial evaluation" in {
+    def func(input: SetExp) = {
+      input match {
+        case Value(x) => Value(x)
+        case Variable(x) => {
+          val output = Variable(x).eval()
+          val re = output match {
+            case p: PartialResult => Variable(x)
+            case a: Any => Value(a)
+          }
+          re
+        }
 
-    AbstractClassDef("AbstractClass1", Private(Field("f1")), Public(CreateMethod("m1", Params("a", "b"))), Public(CreateMethod("m2", Params("a", "b"), Assign("c", Union(Variable("a"), Variable("b"))), Variable("c")))).eval()
-    ClassDef("MyClass1", Private(Field("f1")), Constructor(Assign("f1", Value(5))), Public(CreateMethod("m1", Params("a", "b"), Assign("c", Cross(Variable("a"), Variable("b"))), Variable("c")))).eval()
-    Interface("MyInterface", Public(CreateMethod("m4", Params("a", "b")))).eval()
+        // Optimizations
+        // 1. Intersect(x, x) => Value(x)
+        // 2. Intersect(x, emptySet) => emptySet
+        case Intersect(x, y) => {
+          if(x == y)
+            x.eval()
+          else if(x.eval() == mutable.HashSet() || y.eval() == mutable.HashSet())
+            mutable.HashSet()
+          else
+            Intersect(x, y).eval()
+        }
+      }
+    }
 
-    assert((ClassDef("MyClass1") Implements AbstractClassDef("AbstractClass1")) === "A class can't be implemented. It can only be extended")
-    assert((Interface("MyInterface") Implements ClassDef("MyClass1")) === "Only a class/abstract_class can implement an interface")
-    assert((ClassDef("MyClass1") Implements ClassDef("MyClass1")) === "A class can't be implemented. It can only be extended")
-
+    // Value of a is not present
+    assert(MonadsOptimize(Intersect(Variable("a"), Variable("a"))).map(func) === Variable("a"))
+    assert(MonadsOptimize(Intersect(Variable("a"), Value(mutable.HashSet()))).map(func) === mutable.HashSet())
   }
 }
